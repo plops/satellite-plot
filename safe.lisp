7e5c0032d8dfab8d42ba4207a70dda266af91cae
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-08-07 23:21:16 +0200

    parse binary data into struct

--- safe.lisp
+++ safe.lisp
@@ -9,6 +9,31 @@
 (in-package :g)
 
 (defparameter *fn* (elt (directory "/dev/shm/S*/*-??????.dat") 0))
+;; https://github.com/heegaiximephoomeeghahyaiseekh/lisp-binary/wiki/DEFBINARY
+
+(defbinary packet-primary-header (:byte-order :big-endian)
+  (version 0 :type 3)
+  (type 0 :type 1)
+  (flag 0 :type 1)
+  (application-process-id 0 :type 11)
+  (sequence-flags 0 :type 2)
+  (sequence-count 0 :type 14)
+  (data-length 0 :type 16))
+
+(with-open-binary-file (in *fn* :direction :input)
+  (let* ((file-size (file-length in)))
+    (read-binary 'packet-primary-header in)))
+
+;; => #S(PACKET-PRIMARY-HEADER
+;;       :VERSION 0
+;;       :TYPE 0
+;;       :FLAG 1
+;;       :APPLICATION-PROCESS-ID 1052
+;;       :SEQUENCE-FLAGS 3
+;;       :SEQUENCE-COUNT 3780
+;;       :DATA-LENGTH 15309), 6
+
+
 
 (defparameter *f*
  (open *fn* :direction :input :element-type '(unsigned-byte 8)))
@@ -27,6 +52,5 @@
 ;;  S1 SAR SPPDU [CFI-06] shows further details of the ISP structure
 ;;  Sentinel-1 SAR Space Packet Protocol Data Unit   S1-IF-ASD-PL-0007
 ;; https://sentinel.esa.int/documents/247904/685163/Sentinel-1-SAR-Space-Packet-Protocol-Data-Unit.pdf
-
 
 
