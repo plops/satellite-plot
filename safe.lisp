ee56d9e60db23cb54b008b4b2a7d7ebbfc0fcfa7
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-08-07 21:47:35 +0200

    figure out what cfi-06 is

--- safe.lisp
+++ safe.lisp
@@ -5,7 +5,7 @@
 ;; https://sentinels.copernicus.eu/c/document_library/get_file?folderId=349449&name=DLFE-4502.pdf
 
 ;; measurement data component binary file
-;; containing stream of downlinked ISPs details in 3.3.1.1.2 (p.64)
+;; containing stream of downlinked instrument source packets (ISPs) details in 3.3.1.1.2 (p.64)
 ;; big-endian
 ;; isp-packet = packet-primary-header + packet-data-field
 ;;              6 bytes                 <= 65534 bytes
@@ -14,6 +14,9 @@
 ;;   packet-id (13bits) + packet-sequence-control (2bytes) + packet-data-length (2bytes)
 ;; packet-secondary-header (62bytes) = user-data-field (<= 65472 bytes)
 
-;; CFI-06 shows further details of the ISP structure
+;;  S1 SAR SPPDU [CFI-06] shows further details of the ISP structure
+;;  Sentinel-1 SAR Space Packet Protocol Data Unit   S1-IF-ASD-PL-0007
+;; https://sentinel.esa.int/documents/247904/685163/Sentinel-1-SAR-Space-Packet-Protocol-Data-Unit.pdf
+
 
 
