52f6171b05fc3dfbc8c5b8990b99feff1b996c95
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-08-08 07:49:33 +0200

    ecc number type definition

--- safe.lisp
+++ safe.lisp
@@ -11,18 +11,84 @@
 (defparameter *fn* (elt (directory "/dev/shm/S*/*-??????.dat") 0))
 ;; https://github.com/heegaiximephoomeeghahyaiseekh/lisp-binary/wiki/DEFBINARY
 
-(defbinary packet-primary-header (:byte-order :big-endian)
-  (version 0 :type 3)
-  (type 0 :type 1)
-  (flag 0 :type 1)
-  (application-process-id 0 :type 11)
+
+;; https://sentinel.esa.int/documents/247904/685163/Sentinel-1-SAR-Space-Packet-Protocol-Data-Unit.pdf
+
+(define-enum ecc-number-type 1
+  contingency-0
+  Stripmap-1
+  Stripmap-2
+  Stripmap-3
+  Stripmap-4
+  Stripmap-5n
+  Stripmap-6
+  contingency-7
+  Interferometric-Wide-Swath
+  Wave-Leapfrog-mode
+  Stripmap-5s
+  Stripmap-1-w/o-interl.Cal
+  Stripmap-2-w/o-interl.Cal
+  Stripmap-3-w/o-interl.Cal
+  Stripmap-4-w/o-interl.Cal
+  RFC-mode
+  Test-Mode
+  Elevation-Notch-S3
+  Azimuth-Notch-S1
+  Azimuth-Notch-S2
+  Azimuth-Notch-S3
+  Azimuth-Notch-S4
+  Azimuth-Notch-S5n
+  Azimuth-Notch-S5s
+  Stripmap-5n-w/o-interl.Cal
+  Stripmap-5s-w/o-interl.Cal
+  Stripmap-6-w/o-interl.Cal
+  contingency-28
+  contingency-29
+  contingency-30
+  Elevation-Notch-S3-w/o-interl.Cal
+  Extra-Wide-Swath
+  Azimuth-Notch-S1-w/o-interl.Cal
+  Azimuth-Notch-S3-w/o-interl.Cal
+  Azimuth-Notch-S6-w/o-interl.Cal
+  contingency-36
+  Noise-Characterisation-S1
+  Noise-Characterisation-S2
+  Noise-Characterisation-S3
+  Noise-Characterisation-S4)
+
+(defbinary space-packet (:byte-order :big-endian)
+  ;; start of packet primary header
+  (packet-version-number 0 :type 3)
+  (packet-type 0 :type 1)
+  (secondary-header-flag 0 :type 1)
+  (application-process-id-process-id 0 :type 7)
+  (application-process-id-packet-category 0 :type 4) 
   (sequence-flags 0 :type 2)
-  (sequence-count 0 :type 14)
-  (data-length 0 :type 16))
+  (sequence-count 0 :type 14) ;; 0 at start of measurement, wraps after 16383
+  (data-length 0 :type 16) ;; number of octets in packet data field - 1
+  ;; start of packet data field
+  ;; datation service p. 15
+  (coarse-time 0 :type 32)
+  (fine-time 0 :type 16)
+  ;; fixed ancillary data service
+  (sync-marker #x352EF853
+	       :type (magic :actual-type (unsigned-byte 32)
+			    :value #x352EF853))
+  (data-take-id 0 :type 32)
+  (ecc-number 0 :type 8)
+  (ignore-0 )
+  (test-mode)
+  (rx-channel-id)
+  (instrument-configuration-id)
+  ;; sub-commutation ancillary data service
+  ;; counters service
+  ;; radar configuration support service
+  ;; radar sample count service
+  )
 
 (with-open-binary-file (in *fn* :direction :input)
   (let* ((file-size (file-length in)))
-    (read-binary 'packet-primary-header in)))
+    (read-binary 'space-packet in)))
 
 ;; => #S(PACKET-PRIMARY-HEADER
 ;;       :VERSION 0
