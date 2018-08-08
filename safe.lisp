cd8375dfca17440cfa6dded542befe7a029dcf87
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-08-08 08:09:23 +0200

    parse more of the packet

--- safe.lisp
+++ safe.lisp
@@ -14,7 +14,7 @@
 
 ;; https://sentinel.esa.int/documents/247904/685163/Sentinel-1-SAR-Space-Packet-Protocol-Data-Unit.pdf
 
-(define-enum ecc-number-type 1
+(define-enum ecc-number-type 1 ()
   contingency-0
   Stripmap-1
   Stripmap-2
@@ -26,10 +26,10 @@
   Interferometric-Wide-Swath
   Wave-Leapfrog-mode
   Stripmap-5s
-  Stripmap-1-w/o-interl.Cal
-  Stripmap-2-w/o-interl.Cal
-  Stripmap-3-w/o-interl.Cal
-  Stripmap-4-w/o-interl.Cal
+  Stripmap-1-wo-interl-cal
+  Stripmap-2-wo-interl-cal
+  Stripmap-3-wo-interl-cal
+  Stripmap-4-wo-interl-cal
   RFC-mode
   Test-Mode
   Elevation-Notch-S3
@@ -39,24 +39,38 @@
   Azimuth-Notch-S4
   Azimuth-Notch-S5n
   Azimuth-Notch-S5s
-  Stripmap-5n-w/o-interl.Cal
-  Stripmap-5s-w/o-interl.Cal
-  Stripmap-6-w/o-interl.Cal
+  Stripmap-5n-wo-interl-cal
+  Stripmap-5s-wo-interl-cal
+  Stripmap-6-wo-interl-cal
   contingency-28
   contingency-29
   contingency-30
-  Elevation-Notch-S3-w/o-interl.Cal
+  Elevation-Notch-S3-wo-interl-cal
   Extra-Wide-Swath
-  Azimuth-Notch-S1-w/o-interl.Cal
-  Azimuth-Notch-S3-w/o-interl.Cal
-  Azimuth-Notch-S6-w/o-interl.Cal
+  Azimuth-Notch-S1-wo-interl-cal
+  Azimuth-Notch-S3-wo-interl-cal
+  Azimuth-Notch-S6-wo-interl-cal
   contingency-36
   Noise-Characterisation-S1
   Noise-Characterisation-S2
   Noise-Characterisation-S3
-  Noise-Characterisation-S4)
+  Noise-Characterisation-S4
+  Noise-Characterisation-S5n
+  Noise-Characterisation-S5s
+  Noise-Characterisation-S6
+  Noise-Characterisation-EWS
+  Noise-Characterisation-IWS
+  Noise-Characterisation-Wave)
 
-(defbinary space-packet (:byte-order :big-endian)
+;; p21
+#+nil
+(define-enum test-mode-type 1 ()
+	     (default 0)
+	     (contingency-operational-test #b100)
+	     (contingency-bypass-test #b101)
+	     (oper #b110)
+	     (bypass #b111))
+(defbinary space-packet1 (:byte-order :big-endian)
   ;; start of packet primary header
   (packet-version-number 0 :type 3)
   (packet-type 0 :type 1)
@@ -75,11 +89,11 @@
 	       :type (magic :actual-type (unsigned-byte 32)
 			    :value #x352EF853))
   (data-take-id 0 :type 32)
-  (ecc-number 0 :type 8)
-  (ignore-0 )
-  (test-mode)
-  (rx-channel-id)
-  (instrument-configuration-id)
+  (ecc-number 0 :type ecc-number-type)
+  (ignore-0 0 :type 1)
+  (test-mode 0 :type 3)
+  (rx-channel-id 0 :type 4)
+  (instrument-configuration-id 0 :type 32)
   ;; sub-commutation ancillary data service
   ;; counters service
   ;; radar configuration support service
@@ -88,17 +102,26 @@
 
 (with-open-binary-file (in *fn* :direction :input)
   (let* ((file-size (file-length in)))
-    (read-binary 'space-packet in)))
+    (read-binary 'space-packet1 in)))
 
-;; => #S(PACKET-PRIMARY-HEADER
-;;       :VERSION 0
-;;       :TYPE 0
-;;       :FLAG 1
-;;       :APPLICATION-PROCESS-ID 1052
-;;       :SEQUENCE-FLAGS 3
-;;       :SEQUENCE-COUNT 3780
-;;       :DATA-LENGTH 15309), 6
-
+;; #S(SPACE-PACKET1
+;;    :PACKET-VERSION-NUMBER 0
+;;    :PACKET-TYPE 0
+;;    :SECONDARY-HEADER-FLAG 1
+;;    :APPLICATION-PROCESS-ID-PROCESS-ID 65
+;;    :APPLICATION-PROCESS-ID-PACKET-CATEGORY 12
+;;    :SEQUENCE-FLAGS 3
+;;    :SEQUENCE-COUNT 3780
+;;    :DATA-LENGTH 15309
+;;    :COARSE-TIME 1217599483
+;;    :FINE-TIME 15700
+;;    :SYNC-MARKER 892270675
+;;    :DATA-TAKE-ID 84288864
+;;    :ECC-NUMBER INTERFEROMETRIC-WIDE-SWATH
+;;    :IGNORE-0 0
+;;    :TEST-MODE 0
+;;    :RX-CHANNEL-ID 1
+;;    :INSTRUMENT-CONFIGURATION-ID 6)
 
 
 (defparameter *f*
