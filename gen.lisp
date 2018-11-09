2cb5716679837a74f4fc6d0e53d5afead9066efd
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-11-09 23:58:32 +0100

    more python code

--- gen.lisp
+++ gen.lisp
@@ -10,30 +10,21 @@
        `(do0
 	 (imports (sys
 		   os
-                   (lp loopy)
-                   (cl pyopencl)
                    (np numpy)
-                   pyopencl.array))
-         (setf X (dot (np.random.random (tuple 7000 3))
-                      (astype np.float32)))
-         (setf ctx (cl.create_some_context :interactive False)
-               q (cl.CommandQueue ctx)
-               X_dev (cl.array.to_device q X)
-               )
-         (setf knl (lp.make_kernel (string
-                                    "{[i,j,k]:0<=i,j<M and 0<=k<N}"
-                                    #+nil,(emit-py :code `(dict ((list i j k) (and (<= 0 (tuple i j) (- M 1))
-                                                                                    (<= 0 k (- N 1)))))))
-                                   (string ,(emit-py :code `(= (aref D i j)
-                                                                  (sqrt (sum k (** (- (aref X i k)
-                                                                                      (aref X j k))
-                                                                                   2))))))
-                                   :lang_version (tuple 2018 2))
-               knl (lp.set_options knl :write_cl True)
-               knl (lp.set_loop_priority knl (string "i,j"))
-               result (knl q :X X_dev))
-	 )))
+		   (pd pandas)
+		   (plt matplotlib.pyplot)))
+	 (plt.ion)
+	 (setf df (pd.read_csv (string "/home/martin/sat-data/headers.csv")))
+	 (setf n (* 2 (dot (aref df (string "NUMBER-OF-QUADS"))
+		       (aref iloc 0))))
+         (setf a (dot (np.fromfile (string "/home/martin/sat-data/chunk0")
+			       :dtype np.complex64
+			       :count (* 600 n))
+		      (reshape (tuple 600 n))))
+	 (setf ax (plt.subplot2grid (tuple 1 1) (tuple 0 0)))
+	 (plt.imshow (np.real a))
+	 (ax.set_aspect (string "auto")))))
   ;(run code)
-  (write-source "/home/martin/stage/cl-py-generator/example/03_cl/source/code" code))
+  (write-source "/home/martin/stage/satellite-plot/source/code" code))
 
 
--- source/code.py
+++ source/code.py
@@ -1,4 +1,17 @@
 import sys
 import os
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+plt.ion()
+df=pd.read_csv("/home/martin/sat-data/headers.csv")
+
+n=((2)*(df["NUMBER-OF-QUADS"].iloc[0]))
+
+a=np.fromfile("/home/martin/sat-data/chunk0", dtype=np.complex64, count=((600)*(n))).reshape((600, n))
+
+ax=plt.subplot2grid((1, 1), (0, 0))
+
+plt.imshow(np.real(a))
+ax.set_aspect("auto")
