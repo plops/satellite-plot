3c10ac89f22ebd11d6d432ed78b463e7311290ef
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-11-10 09:12:16 +0100

    try to show fft

--- gen.lisp
+++ gen.lisp
@@ -11,6 +11,7 @@
 	 (imports (sys
 		   os
                    (np numpy)
+		   numpy.fft
 		   (pd pandas)
 		   (plt matplotlib.pyplot)))
 	 (plt.ion)
@@ -20,9 +21,16 @@
          (setf a (dot (np.fromfile (string "/home/martin/sat-data/chunk0")
 			       :dtype np.complex64
 			       :count (* 600 n))
-		      (reshape (tuple 600 n))))
+		      (reshape (tuple 600 n)))
+	       win (np.hamming n)
+	       k (np.fft.fft2 (* win a)
+			      :axes (list 0))
+	       ;(aref k (slice 0 20) ":") 0
+	       )
 	 (setf ax (plt.subplot2grid (tuple 1 1) (tuple 0 0)))
-	 (plt.imshow (np.real a))
+	 (plt.imshow (np.real (* a (+ 1 (* 0 win))))
+		     ;(np.log (+ .001 (np.abs k)))
+		     )
 	 (ax.set_aspect (string "auto")))))
   ;(run code)
   (write-source "/home/martin/stage/satellite-plot/source/code" code))
--- source/code.py
+++ source/code.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import numpy as np
+import numpy.fft
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -10,8 +11,10 @@ df=pd.read_csv("/home/martin/sat-data/headers.csv")
 n=((2)*(df["NUMBER-OF-QUADS"].iloc[0]))
 
 a=np.fromfile("/home/martin/sat-data/chunk0", dtype=np.complex64, count=((600)*(n))).reshape((600, n))
+win=np.hamming(n)
+k=np.fft.fft2(((win)*(a)), axes=[0])
 
 ax=plt.subplot2grid((1, 1), (0, 0))
 
-plt.imshow(np.real(a))
+plt.imshow(np.real(((a)*(((1)+(((0)*(win))))))))
 ax.set_aspect("auto")
