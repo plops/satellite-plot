c92a54edf31bcdaae0900f9d1242c31423474e8c
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-05-31 11:02:39 +0200

    this is very slow

--- gen.lisp
+++ gen.lisp
@@ -12,14 +12,14 @@
        `(do0
 	 (imports (sys
 		   os
-                   (np numpy)
+		   (np numpy)
 		   numpy.fft
 		   (pd pandas)
 		   (plt matplotlib.pyplot)))
 	 (plt.ion)
 	 (setf df_ (pd.read_csv (string ;"/home/martin/sat-data/headers.csv"
-				"/home/martin/stage/satellite-plot/headers.csv"
-				)))
+				 "/home/martin/stage/satellite-plot/headers.csv"
+				 )))
 
 	 (do0
 	  "# rename columns to _ instead of -"
@@ -27,7 +27,7 @@
 	  (for (c df_.columns)
 	       (setf (aref new_names c) (dot c
 					     (replace (string "-")
-							(string "_"))
+						      (string "_"))
 					     (lower))))
 	  (setf df (df_.rename new_names :axis (string "columns"))))
 	 
@@ -49,26 +49,47 @@
 			   (aref iloc 0))))
 	 ;; df['SAB-SSB-ELEVATION-BEAM-ADDRESS'].unique() => array([ 6, 11,  7, 12,  8, 15,  9, 10])
 	 #+nil (setf q (aref (aref df (== 6 (aref df (string "SAB-SSB-ELEVATION-BEAM-ADDRESS"))))
-		       (string "SAB-SSB-AZIMUTH-BEAM-ADDRESS")))
+			     (string "SAB-SSB-AZIMUTH-BEAM-ADDRESS")))
 	 ;; find index where elev beam address changes
 	 ;;df.iloc[np.diff(df[df['SAB-SSB-ELEVATION-BEAM-ADDRESS']==6].index)!=1][0]
 
-	
+	 
 	 
 	 (setf w (aref (dot (aref df.iloc
-			      (!= 1 (np.diff
-				     (dot
-				      (aref df
-					    (== 6
-						df.sab_ssb_elevation_beam_address
-						#+nil
-						(aref df
-						      (string "SAB-SSB-ELEVATION-BEAM-ADDRESS"))))
-				      index))))
+				  (!= 1 (np.diff
+					 (dot
+					  (aref df
+						(== 6
+						    df.sab_ssb_elevation_beam_address
+						    #+nil
+						    (aref df
+							  (string "SAB-SSB-ELEVATION-BEAM-ADDRESS"))))
+					  index))))
 			    index) 0))
 	 (do0
 	  "# relate to tx pulse definition (which is rank pulse repititions before)"
-	  )))
+	  ;; sampling window start
+	  ,@(let ((l `(tx_ramp_rate_magnitude
+		       ;tx_ram_rate_polarity
+		       )))
+	      (loop for e in l collect
+		   (let ((name (format nil "old_~a" e)))
+		    `(do0
+		      (setf (aref df (string ,name)) 0)
+		      (for ((ntuple idx row) (df.iterrows))
+			   (print idx)
+			   (setf (dot (aref df (string ,name)) (aref iloc idx))
+				 (dot (aref df (string ,e)) (aref iloc (- idx (aref row (string "rank")))))))))))))))
+  ;(run code)
+  (write-source "/home/martin/stage/satellite-plot/source/code" code))
+
+;; SC packet-sequence-count, starts with 0, wraps around at 16383
+
+
+
+#+nil
+(do0
+	 #+nil
 	 (do0
 	  (imports ((pg pyqtgraph)
 		    ))
@@ -82,13 +103,15 @@
 	  (setf font (QtGui.QFont))
 	  (font.setPointSize 5)
 	  (widget.setFont font))
+
+	 #+nil
 	 (do0
 	  (setf type_header (list)
 		short_names (list))
 	  (for (c ("list" df.columns))
 	       #+nil (do0 (print (+ (string "col: ") c) )
-		    (print (+ (string "        ") (str (aref (aref df c) 0))))
-		    (print (+ (string "        ") (str (type (aref (aref df c) 0))))))
+			  (print (+ (string "        ") (str (aref (aref df c) 0))))
+			  (print (+ (string "        ") (str (type (aref (aref df c) 0))))))
 	       (setf example (aref (aref df c) 0)
 		     example_type (type example)
 		     short_name (dot (string "")
@@ -103,8 +126,9 @@
 	       (print (dot (string "{} .. {}")
 			   (format new_short_name c)))
 	       (short_names.append new_short_name)
-	       (setf v (tuple c ;new_short_name
-			      example_type))
+	       (setf v (tuple ;c ;
+			new_short_name
+			example_type))
 	       
 	       (type_header.append v))
 	  (do0 ;; FIXME: ECC-NUMBER is a string but is not copied
@@ -113,30 +137,37 @@
 	   (setf df1 (aref df
 			   (!= 0.0 (dot df.sab_ssb_elevation_beam_address
 					#+nil (aref df
-					      (string "SAB-SSB-ELEVATION-BEAM-ADDRESS"))
+						    (string "SAB-SSB-ELEVATION-BEAM-ADDRESS"))
 					(diff))))
 		 ;; also get index behind and before change
 		 df1i (aref (sorted (+ ("list"
-				   (- df1.index 1)
-				   )
-				  ("list"
-				   df1.index
-				   )
-				  ("list"
-				   (+ df1.index 1)
-				   )))
+					(- df1.index 1)
+					)
+				       ("list"
+					df1.index
+					)
+				       ("list"
+					(+ df1.index 1)
+					)))
 			    "1:-1"))
 	   (for ((ntuple idx row)
 		 (dot (aref df.iloc df1i) (iterrows)) )
 		(contents.append ("tuple" row))
 		#+nil (if (< 1000 idx)
-		    break)))
+			  break)))
 	  (setf data (np.array contents
-				   :dtype type_header)))
+			       :dtype type_header))
+
+	  )
 	 (widget.setData data)
+	 
+	 
+	 
+	 
+	 
 	 #+nil
 	 (do0
-          (setf a (dot (np.fromfile (string "/home/martin/sat-data/chunk0")
+	  (setf a (dot (np.fromfile (string "/home/martin/sat-data/chunk0")
 				    :dtype np.complex64
 				    :count (* w n))
 		       (reshape (tuple w n)))
@@ -150,8 +181,3 @@
 					;(np.log (+ .001 (np.abs k)))
 		      )
 	  (ax.set_aspect (string "auto"))))
-	))
-  ;(run code)
-  (write-source "/home/martin/stage/satellite-plot/source/code" code))
-
-;; SC packet-sequence-count, starts with 0, wraps around at 16383
--- source/code.py
+++ source/code.py
@@ -19,35 +19,8 @@ attitude_data=sub_data[22:41]
 temperature_hk_data=sub_data[41:64]
 n=((2)*(df.number_of_quads.iloc[0]))
 w=df.iloc[((1)!=(np.diff(df[((6)==(df.sab_ssb_elevation_beam_address))].index)))].index[0]
-import pyqtgraph as pg
-from pyqtgraph.Qt import QtCore, QtGui
-app=QtGui.QApplication([])
-widget=pg.TableWidget(sortable=False)
-widget.show()
-widget.resize(500, 500)
-widget.setWindowTitle("satellite data header")
-font=QtGui.QFont()
-font.setPointSize(5)
-widget.setFont(font)
-type_header=[]
-short_names=[]
-for c in list(df.columns):
-    example=df[c][0]
-    example_type=type(example)
-    short_name="".join(map(lambda x: x[0], c.split("_")))
-    new_short_name=short_name
-    count=0
-    while ((new_short_name in short_names)):
-        count=((1)+(count))
-        new_short_name=((short_name)+(str(count)))
-    print("{} .. {}".format(new_short_name, c))
-    short_names.append(new_short_name)
-    v=(c,example_type,)
-    type_header.append(v)
-contents=[]
-df1=df[(((0.0e+0))!=(df.sab_ssb_elevation_beam_address.diff()))]
-df1i=sorted(((list(((df1.index)-(1))))+(list(df1.index))+(list(((df1.index)+(1))))))[1:-1]
-for idx, row in df.iloc[df1i].iterrows():
-    contents.append(tuple(row))
-data=np.array(contents, dtype=type_header)
-widget.setData(data)
+# relate to tx pulse definition (which is rank pulse repititions before)
+df["old_tx_ramp_rate_magnitude"]=0
+for idx, row in df.iterrows():
+    print(idx)
+    df["old_tx_ramp_rate_magnitude"].iloc[idx]=df["tx_ramp_rate_magnitude"].iloc[((idx)-(row["rank"]))]
